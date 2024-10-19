4953994f494a5e08494071ffc9d5630e918ada56
willsondev <willsondev12@gmail.com>
2024-10-19 11:49:37 -0300

    cambios pruebas

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Configuración de Firebase
 const firebaseConfig = {
-  apiKey: import.meta.env.VUE_APP_API_KEY,
-  authDomain: import.meta.env.VUE_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.VUE_APP_PROJECT_ID,
-  storageBucket: import.meta.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VUE_APP_APP_ID
+  apiKey: "AIzaSyCh1171Mf8YXpa_mP6inxzLJqp9RDYQ588",
+  authDomain: "votaciones-70f6d.firebaseapp.com",
+  projectId: "votaciones-70f6d",
+  storageBucket:"votaciones-70f6d.appspot.com",
+  messagingSenderId: "1043395963178",
+  appId: "1:1043395963178:web:8c37f37101c525d1785653"
 };
 
 const app = initializeApp(firebaseConfig);
