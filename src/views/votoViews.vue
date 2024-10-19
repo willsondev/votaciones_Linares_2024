76735a2e90a582deaebcbd63bcf0b03b2fe9bdf4
willsondev <willsondev12@gmail.com>
2024-10-19 11:58:49 -0300

    imagen-candidato

--- src/views/votoViews.vue
+++ src/views/votoViews.vue
@@ -42,23 +42,30 @@
 
     <!-- Sección de candidatos -->
     <div v-if="filteredData.length > 0">
-      <div class="grid grid-cols-2 gap-6 mt-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        <div class="w-full p-4 bg-white border rounded-lg shadow-md" v-for="(item, index) in filteredData" :key="index">
-          <div class="flex flex-col items-start mb-4">
-            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900">{{ item.Nombre }} {{ item['Primer  Apellido'] }} {{ item['Segundo  Apellido'] }}</h5>
-            <p class="font-bold text-red-500">{{ item['Tipo  Eleccion'] }}</p>
-          </div>
-          <div class="flex items-center justify-between">
-            <button 
-              @click="votar(item)" 
-              class="w-full font-bold text-white bg-gradient-to-r from-red-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 rounded-lg text-sm px-19 py-2.5 text-center me-2 mb-2"
-            >
-              Votar
-            </button>
-          </div>
-        </div>
+  <div class="grid grid-cols-2 gap-6 mt-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div class="w-full p-4 bg-white border rounded-lg shadow-md" v-for="(item, index) in filteredData" :key="index">
+      <div class="flex flex-col items-start mb-4">
+        <!-- Imagen del candidato -->
+        <img 
+          :src="item.img" 
+          alt="Imagen del candidato" 
+          class="w-16 h-16 mb-2 rounded-full" 
+        />
+        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900">{{ item.Nombre }} {{ item['Primer  Apellido'] }} {{ item['Segundo  Apellido'] }}</h5>
+        <p class="font-bold text-red-500">{{ item['Tipo  Eleccion'] }}</p>
+      </div>
+      <div class="flex items-center justify-between">
+        <button 
+          @click="votar(item)" 
+          class="w-full font-bold text-white bg-gradient-to-r from-red-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 rounded-lg text-sm px-19 py-2.5 text-center me-2 mb-2"
+        >
+          Votar
+        </button>
       </div>
     </div>
+  </div>
+</div>
+
 
     <!-- Mensaje cuando no hay datos -->
     <div v-else>
