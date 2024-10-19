459b9178c18f57dc9c46a42519a9dddadb8d73a7
willsondev <willsondev12@gmail.com>
2024-10-19 12:11:09 -0300

    pwg

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css'
 import router from './router'; // Importa el router
+import './registerServiceWorker'
 
 createApp(App).use(router).mount('#app')
